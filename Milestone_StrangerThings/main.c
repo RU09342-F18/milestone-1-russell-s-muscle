795c5d3036782c14cffd05c560a012b0e34dbbfc
prices0 <35497779+prices0@users.noreply.github.com>
2018-10-17 16:12:57 -0400

    Update main.c
    Added uart configuration and updated some errors

--- Milestone_StrangerThings/main.c
+++ Milestone_StrangerThings/main.c
@@ -1,4 +1,6 @@
-#include <msp430.h> 
+//https://www.embeddedrelated.com/showarticle/420.php
+//TI REsource explorer msp430g2xx3_uscia0_uart_01_9600.c
+#include <msp430.h>
 
 
 /**
@@ -9,8 +11,8 @@ int Input = 0;
 int count = 0;
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
 
 }
 
@@ -46,15 +48,22 @@ void configureTimer ()
     TA1CCR1 = 0;
     TA1CCR2 = 0;
 
-    TA0CCTL = OUTMOD_3;
-    TA1CCTL = OUTMOD_3;
-    TA2CCTL = OUTMOD_3;
+    TA0CCTL1 = OUTMOD_3;
+    TA1CCTL1 = OUTMOD_3;
+    TA1CCTL2 = OUTMOD_3;
 
 }
 
 void configureUART ()
 {
-    
+    P1SEL |= BIT1+BIT2;
+    P1SEL2 |= BIT1+BIT2;
+    UCA0CTL1 |= UCSSEL_2; // SMCLK
+    UCA0BR0 = 9600; //sets to specified baud rate of 9600
+    UCA0BR1 = 9600; //sets to specified baud rate of 9600
+    UCA0MCTL = UCBRS2 + UCBRS0; // Modulation UCBRSx = 5
+    UCA0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
+    UC0IE |= UCA0RXIE; // Enable USCI_A0 RX interrupt
     UCOIE |=UCARXIE;
 }
 
@@ -65,21 +74,21 @@ __interrupt void USCI0RX_ISR(void) {
 
     UC0IE |= UCA0TXIE;
     Input |= UCA0RXBUF;
-    
+
     switch (count)
-    
+
     case (0)
     commandlength |= Input;
     break;
     case (1)
-    
-    
+
+
 }
 
 #pragma vector=USCIAB0TX_VECTOR
 __interrupt void USCI0TX_ISR(void) {
      P1OUT |= BIT0;
      P1OUT &= ~BIT0;
-     
+
      UCOIE &= ~UCA0TXIE;
 }
