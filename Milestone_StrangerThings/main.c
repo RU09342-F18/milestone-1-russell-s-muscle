229b72c0e619b9f210c7d784d2ba8a83fe8e8c8a
prices0 <35497779+prices0@users.noreply.github.com>
2018-10-17 20:06:04 -0400

    Update main.c
    IT WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Milestone_StrangerThings/main.c
+++ Milestone_StrangerThings/main.c
@@ -6,32 +6,28 @@
 /**
  * main.c
  */
-int commandlength = 0;
-int Input = 0;
-int count = 0;
-int main(void)
-{
-    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-    configureLED();
-    configureTimer();
-    configureUART();
-    __bis_SR_register(LPM0_bits + GIE);
+char commandlength;
+char count;
 
-}
 
 void configureLED ()
 {
     //P1.6 = Red LED
-    //P2.0 = Green LED
+    //P2.1 = Green LED
     //P2.4 = Blue LED
+
+    //determined in family guide that
+    //2.1 is for XXXX
+    //1.6 is for xxxx
+    //2.4 is for xxxx
 
     P1SEL |= BIT6;
     P1SEL2 &= ~BIT6;
     P1DIR |= BIT6;
 
-    P2SEL |= BIT0;
-    P2SEL2 &= ~BIT0;
-    P2DIR |= BIT0;
+    P2SEL |= BIT1;
+    P2SEL2 &= ~BIT1;
+    P2DIR |= BIT1;
 
     P2SEL |= BIT4;
     P2SEL2 &= ~BIT4;
@@ -47,9 +43,11 @@ void configureTimer ()
     TA1CTL = TASSEL_2 + MC_1 + ID_2 + TACLR;
 
     TA0CCR0 = 255;
+    TA1CCR0 = 255;
     TA0CCR1 = 0;
     TA1CCR1 = 0;
     TA1CCR2 = 0;
+
 
     TA0CCTL1 = OUTMOD_3;
     TA1CCTL1 = OUTMOD_3;
@@ -59,62 +57,71 @@ void configureTimer ()
 
 void configureUART ()
 {
- P1SEL |= BIT1+BIT2;
+    P1SEL |= BIT1+BIT2;
     P1SEL2 |= BIT1+BIT2;
     UCA0CTL1 |= UCSSEL_2; // SMCLK
-    UCA0BR0 = 9600; //sets to specified baud rate of 9600
+    UCA0BR0 = 104; //sets to specified baud rate of 9600
     UCA0BR1 = 0; //sets to specified baud rate of 9600
     UCA0MCTL = UCBRS2 + UCBRS0; // Modulation UCBRSx = 5
     UCA0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
     UC0IE |= UCA0RXIE; // Enable USCI_A0 RX interrupt
 }
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    configureLED();
+    configureTimer();
+    configureUART();
+    __bis_SR_register(LPM0_bits + GIE);
+
+}
 
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void) {
+    //int commandlength = 0; =total
+    //int count = 0; =counter
+    //input = data
 
     P2OUT |= BIT5;
-
     UC0IE |= UCA0TXIE;
-    Input |= UCA0RXBUF;
+   char Input = UCA0RXBUF;
 
-    switch (count){
-
-    case (0):
-    commandlength |= Input;
-
-    if(commandlength >= 8){
-
-    UCA0TXBUF = commandlength - 3;
-
+if (count == 0)
+{
+    count = Input;
+    commandlength = Input;
+    if (Input>=8)
+    {
+        UCA0TXBUF = Input - 3;
     }
-    break;
-
-    case (1):
-        TA0CCR1 = Input;
-        break;
-
-    case (2):
-        TA1CCR1 = Input;
-        break;
-
-    case (3):
-        TA1CCR2 = Input;
-        break;
-
-    default:
-        if(commandlength - 3 >= 8)
-        {
+}
+else if (commandlength - count ==1)
+{
+    TA0CCR1 = Input;
+}
+else if(commandlength - count ==2)
+{
+    TA1CCR1=Input;
+}
+else if(commandlength -count ==3)
+{
+    TA1CCR2 = Input;
+}
+else
+{
+    if(commandlength>=8)
+    {
         UCA0TXBUF = Input;
-        }
-        break;
     }
-     P2OUT &= ~BIT5;
+}
+count --;
+P2OUT &= ~BIT5;
 }
 
 #pragma vector=USCIAB0TX_VECTOR
 __interrupt void USCI0TX_ISR(void) {
      P1OUT |= BIT0;
-     P1OUT &= ~BIT0;
 
      UC0IE &= ~UCA0TXIE;
+     P1OUT &= ~BIT0;
 }
