8b1e846240fee0ce26558e35d096fe50fb5513b4
prices0 <35497779+prices0@users.noreply.github.com>
2018-10-19 08:17:48 -0400

    Needs comments

--- Milestone_StrangerThings/main.c
+++ Milestone_StrangerThings/main.c
@@ -16,6 +16,11 @@ void configureLED ()
     //P2.1 = Green LED
     //P2.4 = Blue LED
 
+    //determined in family guide that
+    //2.1 is for XXXX
+    //1.6 is for xxxx
+    //2.4 is for xxxx
+
     P1SEL |= BIT6;
     P1SEL2 &= ~BIT6;
     P1DIR |= BIT6;
@@ -55,16 +60,16 @@ void configureUART ()
     P1SEL |= BIT1+BIT2;
     P1SEL2 |= BIT1+BIT2;
     UCA0CTL1 |= UCSSEL_2; // SMCLK
-    UCA0BR0 = 104; //sets to specified baud rate of 9600
+    UCA0BR0 = 109; //sets to specified baud rate of 9600
     UCA0BR1 = 0; //sets to specified baud rate of 9600
-    UCA0MCTL = UCBRS2 + UCBRS0; // Modulation UCBRSx = 5
+    UCA0MCTL = UCBRS_2; // Modulation UCBRSx = 5
     UCA0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
     UC0IE |= UCA0RXIE; // Enable USCI_A0 RX interrupt
 }
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-    configureLED();
+
     configureTimer();
     configureUART();
     __bis_SR_register(LPM0_bits + GIE);
@@ -73,37 +78,38 @@ int main(void)
 
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void) {
+    configureLED();
     P2OUT |= BIT5;
     UC0IE |= UCA0TXIE;
-   char Input = UCA0RXBUF;
+   char input = UCA0RXBUF;
+
 
 if (count == 0)
 {
-    count = Input;
-    commandlength = Input;
-    if (Input>=8)
-    {
-        UCA0TXBUF = Input - 3;
-    }
+    count = input;
+    commandlength = input;
+
+        UCA0TXBUF = input - 3;
+
+
 }
 else if (commandlength - count ==1)
 {
-    TA0CCR1 = Input;
+    TA0CCR1 = input;
 }
 else if(commandlength - count ==2)
 {
-    TA1CCR1=Input;
+    TA1CCR1=input;
 }
 else if(commandlength -count ==3)
 {
-    TA1CCR2 = Input;
+    TA1CCR2 = input;
 }
 else
 {
-    if(commandlength>=8)
-    {
-        UCA0TXBUF = Input;
-    }
+
+        UCA0TXBUF = input;
+
 }
 count --;
 P2OUT &= ~BIT5;
